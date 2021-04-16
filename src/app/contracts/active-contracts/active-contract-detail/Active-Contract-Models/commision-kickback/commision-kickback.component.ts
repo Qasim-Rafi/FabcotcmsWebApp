42c851106210583d90f7fdfb48e1c381632acf7c
Xani595 <itszk6@gmail.com>
2021-04-16 14:08:29 +0500

    dashboard forms

--- src/app/contracts/active-contracts/active-contract-detail/Active-Contract-Models/commision-kickback/commision-kickback.component.ts
+++ src/app/contracts/active-contracts/active-contract-detail/Active-Contract-Models/commision-kickback/commision-kickback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-commision-kickback',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./commision-kickback.component.css']
 })
 export class CommisionKickbackComponent implements OnInit {
+  public data: any[] = [{
+    id: 0
+  }];
+  constructor(
+    private _NgbActiveModal: NgbActiveModal
 
-  constructor() { }
+  ) { }
 
   ngOnInit(): void {
   }
-
+  get activeModal() {
+    return this._NgbActiveModal;
+  }
+  addMore() {
+    this.data.push({
+      id: this.data.length,
+    });
+  }
+  remove(i: number) {
+    this.data.splice(i, 1);
+  }
 }
--- src/app/contracts/active-contracts/active-contract-detail/Active-Contract-Models/remarks/remarks.component.ts
+++ src/app/contracts/active-contracts/active-contract-detail/Active-Contract-Models/remarks/remarks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-remarks',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemarksComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _NgbActiveModal: NgbActiveModal
+  ) { }
 
   ngOnInit(): void {
   }
-
+  get activeModal() {
+    return this._NgbActiveModal;
+  }
 }
