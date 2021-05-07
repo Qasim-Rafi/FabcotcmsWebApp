2f50ed3bbc187cc193d2eae60671c71c3f63fcd9
mluqmanbabar124 <68840838+mluqmanbabar124@users.noreply.github.com>
2021-05-07 15:44:36 +0500

    updates Inovice Details

--- src/app/contracts/active-contracts/active-contract-detail/Active-Contract-Models/sale-invoice-item/sale-invoice-item.component.ts
+++ src/app/contracts/active-contracts/active-contract-detail/Active-Contract-Models/sale-invoice-item/sale-invoice-item.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { ServiceService } from 'src/app/shared/service.service';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-sale-invoice-item',
   templateUrl: './sale-invoice-item.component.html',
@@ -12,6 +12,11 @@ import { ServiceService } from 'src/app/shared/service.service';
 })
 export class SaleInvoiceItemComponent implements OnInit {
   @Input() statusCheck;
+  @Input() contractId;
+  @Input() contractSaleInvoiceId;
+  data:any ={};
+  response: any;
+  itemData:any=[];
   constructor(
     private http: HttpClient,
     private toastr: ToastrService,
@@ -21,9 +26,64 @@ export class SaleInvoiceItemComponent implements OnInit {
 
   ngOnInit(): void {
     this.statusCheck = this.statusCheck;
-
+this.getdropdown();
   }
+
+  addSaleInvoiceItem() {
+    // this.data.saleInvoiceDate = this.dateformater.toModel(this.data.saleInvoiceDate);
+   let varr = {
+
+     "contractSaleInvoiceId": this.contractSaleInvoiceId,
+     "saleInvoiceNo": this.data.saleInvoiceNo,
+     "saleInvoiceDate":this.data.saleInvoiceDate,
+     "saleInvoiceRemarks":this.data.saleInvoiceRemarks
+   }
+
+   this.http.
+     post(`${environment.apiUrl}/api/Contracts/AddSaleInvoiceItem`, varr)
+     .subscribe(
+       res => {
+
+         this.response = res;
+         if (this.response.success == true) {
+           this.toastr.success(this.response.message, 'Message.');
+           this.activeModal.close(true);
+        
+         }
+         else {
+           this.toastr.error(this.response.message, 'Message.');
+         }
+
+       }, err => {
+         if (err.status == 400) {
+           this.toastr.error(this.response.message, 'Message.');
+         }
+       });
+ }
+
+
+
+
   get activeModal() {
     return this._NgbActiveModal;
+  }
+  getdropdown() {
+    this.http.get(`${environment.apiUrl}/api/Lookups/ContractSaleInvoiceItems/` + this.contractId)
+      .subscribe(
+        res => {
+          this.response = res;
+          if (this.response.success == true) {
+            this.itemData = this.response.data;
+            
+          }
+          else {
+            this.toastr.error(this.response.message, 'Message.');
+          }
+  
+        }, err => {
+          if (err.status == 400) {
+            this.toastr.error(this.response.message, 'Message.');
+          }
+        });
   }
 }
