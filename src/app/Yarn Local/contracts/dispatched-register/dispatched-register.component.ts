bbeeadf93056d65eea74e2b8edaa7447405f120c
Samanrafiq <samanrafique668@gmail.com>
2021-06-03 00:17:15 -0700

    changes

---
+++ src/app/Yarn Local/contracts/dispatched-register/dispatched-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispatched-register',
+  templateUrl: './dispatched-register.component.html',
+  styleUrls: ['./dispatched-register.component.css']
+})
+export class DispatchedRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
