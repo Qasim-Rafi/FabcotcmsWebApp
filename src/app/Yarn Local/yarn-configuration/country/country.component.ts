56af55596f569742ea53ab432b2c3d8a9a17f1b1
Xani595 <itszk6@gmail.com>
2021-06-03 17:30:29 +0500

    Yarn-configuration

---
+++ src/app/Yarn Local/yarn-configuration/country/country.component.ts
@@ -0,0 +1,304 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EditCountryComponent } from './edit-country/edit-country.component';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import { ServiceService } from 'src/app/shared/service.service'
+import { ClipboardService } from 'ngx-clipboard';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { GlobalConstants } from 'src/app/Common/global-constants';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+@Component({
+  selector: 'app-country',
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.css']
+})
+
+export class CountryComponent implements OnInit {
+  countryCount: number;
+  response: any;
+  rows: any = [];
+  columns: any = [];
+  data: any = {};
+  copyData: any = [];
+  currentDate = Date.now();
+  countryFilter: any = [];
+  CountryUrl = '/api/Configs/GetAllCountry'
+
+  @ViewChild('myTable', { static: false }) table: DatatableComponent;
+
+  constructor(private http: HttpClient,
+    private toastr: ToastrService,
+    private modalService: NgbModal,
+    private service: ServiceService,
+    private _clipboardService: ClipboardService) { }
+
+  ngOnInit(): void {
+    this.service.fetch((data) => {
+      this.rows = data;
+      this.countryFilter = [...this.rows];
+
+      this.countryCount = this.rows.length;
+    }, this.CountryUrl);
+
+  }
+
+
+  // ------------------- Search function ----------------------------------//
+  search(event) {
+    const val = event.target.value.toLowerCase();
+    const temp = this.countryFilter.filter(function (d) {
+      return (d.name.toLowerCase().indexOf(val) !== -1 || !val);
+    });
+    this.rows = temp;
+  }
+
+  //  --------------------- Delete Country ---------------------------//
+
+  deleteCountry(id) {
+
+    Swal.fire({
+      title: GlobalConstants.deleteTitle, //'Are you sure?',
+      text: GlobalConstants.deleteMessage + ' ' + '"' + id.name + '"',
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonColor: '#ed5565',
+      cancelButtonColor: '#dae0e5',
+      cancelButtonText: 'No',
+      confirmButtonText: 'Yes',
+      reverseButtons: true,
+      position: 'top',
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.http.delete(`${environment.apiUrl}/api/Configs/DeleteCountry/` + id.id)
+          .subscribe(
+            res => {
+              this.response = res;
+              if (this.response.success == true) {
+                this.toastr.error(GlobalConstants.deleteSuccess, 'Message.');
+                this.service.fetch((data) => {
+                  this.rows = data;
+                }, this.CountryUrl);
+
+              }
+              else {
+                this.toastr.error(GlobalConstants.exceptionMessage, 'Message.');
+              }
+
+            }, err => {
+              if (err.status == 400) {
+                this.toastr.error(this.response.message, 'Message.');
+              }
+            });
+      }
+    })
+
+  }
+
+  //  ----------------------- Add country Form -----------------------//
+
+  addCountryForm(check, name) {
+    const modalRef = this.modalService.open(EditCountryComponent, { centered: true });
+    modalRef.componentInstance.statusCheck = check;
+    modalRef.componentInstance.FormName = name;
+    modalRef.result.then((data) => {
+
+      if (data == true) {
+        this.service.fetch((data) => {
+          this.rows = data;
+      this.countryFilter = [...this.rows];
+
+          this.countryCount = this.rows.length;
+        }, this.CountryUrl);
+      }
+    }, (reason) => {
+    });
+  }
+
+  // ---------------------- Edit Country Form ----------------------//
+
+
+  editCountryForm(row, check, name) {
+    const modalRef = this.modalService.open(EditCountryComponent, { centered: true });
+    modalRef.componentInstance.countryId = row.id;
+    modalRef.componentInstance.statusCheck = check;
+    modalRef.componentInstance.FormName = name;
+    modalRef.result.then((data) => {
+      // on close
+      if (data == true) {
+        //  this.date = this.myDate;
+        this.service.fetch((data) => {
+          this.rows = data;
+        }, this.CountryUrl);
+
+      }
+    }, (reason) => {
+      // on dismiss
+    });
+  }
+
+
+  // --------------------------Export as Excel file----------------------------------//
+
+
+  countryExcelFile(){
+    const filtered = this.rows.map(row => ({
+      Sno: row.id,
+      CountryName: row.name,
+      Details: row.details,
+      Status: row.active == true ? "Active" : "In-Active",
+      CreatedOn: row.createdDateTime + ' | ' + row.createdByName
+    }));
+
+    this.service.exportAsExcelFile(filtered, 'Countries');
+
+  }
+
+// -------------------------------- Export as CSV file --------------------------------//
+
+countryCsvFile(){
+  const filtered = this.rows.map(row => ({
+    Sno: row.id,
+    CountryName: row.name,
+    Details: row.details,
+    Status: row.active == true ? "Active" : "In-Active",
+    CreatedOn: row.createdDateTime + ' | ' + row.createdByName
+  }));
+
+  this.service.exportAsCsvFile(filtered, 'Countries');
+
+}
+
+  // -------------------------------Export as Pdf  ------------------------------------//
+
+  countryPdf() {
+
+    let docDefinition = {
+      pageSize: 'A4',
+      info: {
+        title: 'Country List'
+      },
+      content: [
+        {
+          text: 'Country List',
+          style: 'heading',
+
+        },
+
+        {
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [30, 90, 130, 50, 150],
+            body: [
+              ['S.no.', 'Country', 'Details', 'Status', 'Created On| Created By'],
+              ...this.rows.map(row => (
+                [row.id, row.name, row.details,
+                row.active == true ? "Active" : "In-Active", row.createdDateTime + '|' + row.createdByName]
+              ))
+            ]
+          }
+        }
+      ],
+      styles: {
+        heading: {
+          fontSize: 18,
+          alignment: 'center',
+          margin: [0, 15, 0, 30]
+        }
+      }
+
+    };
+
+
+    pdfMake.createPdf(docDefinition).download('CountryList.pdf');
+  }
+
+  //-------------------------------------- Print country List ------------------------- ///
+
+  printCountryList() {
+
+    let docDefinition = {
+      pageSize: 'A4',
+      info: {
+        title: 'Country List'
+      },
+      content: [
+        {
+          text: 'Country List',
+          style: 'heading',
+
+        },
+
+        {
+          layout: 'lightHorizontalLines',
+          table: {
+            headerRows: 1,
+            widths: [30, 90, 130, 50, 150],
+            body: [
+              ['S.no.', 'Country', 'Details', 'Status', 'Created On| Created By'],
+              ...this.rows.map(row => (
+                [row.id, row.name, row.details,
+                row.active == true ? "Active" : "In-Active", row.createdDateTime + '|' + row.createdByName]
+              ))
+            ]
+          }
+        }
+      ],
+      styles: {
+        heading: {
+          fontSize: 18,
+          alignment: 'center',
+          margin: [0, 15, 0, 30]
+        }
+      }
+    };
+
+    // const win = window.open('', "tempWinForPdf");
+    pdfMake.createPdf(docDefinition).print();
+
+  }
+
+
+  //------------------------------------ Copy Country list --------------------///
+
+  copyCountryList() {
+    let count1 = this.rows.map(x => x.name.length);
+    let max1 = count1.reduce((a, b) => Math.max(a, b));
+    let count3 = this.rows.map(x => x.details.length);
+    let max3 = count3.reduce((a, b) => Math.max(a, b));
+    let count4 = this.rows.map(x => x.active == true ? "Active".length : "In-Active".length);
+    let max4 = count4.reduce((a, b) => Math.max(a, b));
+    max1 = max1 + 10;
+    max3 = max3 + 10;
+    max4 = max4 + 10;
+
+    // ................................................ headings replace yours............................
+
+    this.copyData.push('S No.' + 'Country Name'.padEnd(max1) + 'Details'.padEnd(max3) + 'Status'.padEnd(max4) + 'Changed On' + '| Changed By \n');
+    // ................................................ headings............................
+
+    // ................................................ coloum data...........replace your coloum names.................
+    for (let i = 0; i < this.rows.length; i++) {
+      let tempData = this.rows[i].id + this.rows[i].name.padEnd(max1) + this.rows[i].details.padEnd(max3)
+        + this.rows[i].active
+        + this.rows[i].createdDateTime + this.rows[i].createdByName + '\n';
+      this.copyData.push(tempData);
+    }
+    this._clipboardService.copy(this.copyData)
+    // ............................row.active == true ? "Active" : "In-Active".................... coloum this.data............................
+
+    Swal.fire({
+      title: GlobalConstants.copySuccess,
+      footer: 'Copied' + '\n' + this.countryCount + '\n' + 'rows to clipboard',
+      showConfirmButton: false,
+      timer: 2000,
+    })
+  }
+
+}
---
+++ src/app/Yarn Local/yarn-configuration/currency/edit-currency/edit-currency.component.ts
@@ -0,0 +1,103 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgForm } from '@angular/forms';
+import { ServiceService } from 'src/app/shared/service.service';
+import { Dateformater } from 'src/app/shared/dateformater';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+
+  selector: 'app-edit-currency',
+  templateUrl: './edit-currency.component.html',
+  styleUrls: ['./edit-currency.component.css']
+
+})
+export class EditCurrencyComponent implements OnInit {
+  dateformater: Dateformater = new Dateformater();
+  data:any={};
+  response: any;
+  @Input() userId;
+  
+  constructor(private http:HttpClient,
+    private service: ServiceService,
+    private toastr: ToastrService,
+    private _NgbActiveModal: NgbActiveModal ) { }
+
+  ngOnInit(): void {
+    this.editCurrency();
+  }
+  get activeModal() {
+    return this._NgbActiveModal;
+  }
+
+
+
+  editCurrency()
+  {
+    this.http.get(`${environment.apiUrl}/api/Configs/GetCurrencyRateById/`+this.userId )
+    .subscribe(
+      res=> { 
+        this.response = res;
+        if (this.response.success == true){
+          this.data =this.response.data; 
+    this.data.validFrom = this.dateformater.fromModel(this.data.validFrom);
+
+        }
+        else {
+          this.toastr.error('Something went Worng', 'Message.');
+            }
+
+      }, (err: HttpErrorResponse) => {
+        const messages = this.service.extractErrorMessagesFromErrorResponse(err);
+        this.toastr.error(messages.toString(), 'Message.');
+        console.log(messages);
+        // if (err.status == 400) {
+        //   this.toastr.error(this.response.message, 'Message.');
+        // }
+      });
+  }
+
+
+  UpdateCurrency(form:NgForm)
+  {
+    this.data.validFrom = this.dateformater.toModel(this.data.validFrom);
+    if (form.status == "INVALID") {
+
+      this.toastr.error("Invalid Form", 'Message.');
+    }
+    else{
+    let varr=  {
+      "validFrom": this.data.validFrom,
+      "currencyCode":  this.data.currencyCode,
+      "rate": this.data.rate,
+      "details": this.data.details
+    }
+
+    this.http.
+    put(`${environment.apiUrl}/api/Configs/UpdateCurrencyRate/`+this.userId,varr)
+    .subscribe(
+      res=> { 
+  
+        this.response = res;
+        if (this.response.success == true){
+          this.toastr.success(this.response.message, 'Message.');
+          this.activeModal.close(true);
+        }
+        else {
+          this.toastr.error('Something went Worng', 'Message.');
+            }
+
+      }, (err: HttpErrorResponse) => {
+        const messages = this.service.extractErrorMessagesFromErrorResponse(err);
+        this.toastr.error(messages.toString(), 'Message.');
+        console.log(messages);
+        // if (err.status == 400) {
+        //   this.toastr.error(this.response.message, 'Message.');
+        // }
+      });
+  }
+}
+}
