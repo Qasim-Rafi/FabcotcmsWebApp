1ebb1f63e534149344c2adf9874bd8c6e2a7202d
Samanrafiq <samanrafique668@gmail.com>
2021-06-07 00:38:57 -0700

    changes

--- src/app/Yarn Local/contracts/yarn-active-contracts/yarn-active-contracts.component.ts
+++ src/app/Yarn Local/contracts/yarn-active-contracts/yarn-active-contracts.component.ts
@@ -38,7 +38,10 @@ export class YarnActiveContractsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-
+    this.fetch((data) => {
+      this.temp = [...data]; 
+      this.rows = data;
+    });
   }
 
 
@@ -106,84 +109,84 @@ on_HandContract(){
 
 
 
-// fetch(cb) {
+fetch(cb) {
 
-//   this.http
-//     .get(`${environment.apiUrl}/api/Contracts/GetAllContract`)
-//     .subscribe(res => {
-//       this.response = res;
+  this.http
+    .get(`${environment.apiUrl}/api/YarnContracts/GetAllContract`)
+    .subscribe(res => {
+      this.response = res;
 
-//       if (this.response.success == true) {
-//         this.data = this.response.data.list;
-//         this.allCount = this.response.data.allCount;
-//         this.openCount = this.response.data.openCount;
-//         this.closedCount = this.response.data.closedCount;
-//         this.billAwaitedCount = this.response.data.billAwaitedCount;
-//         this.billedCount = this.response.data.billedCount;
-//         this.receivableCount = this.response.data.receivableCount;
-//         this.receivedCount = this.response.data.receivedCount;
-//         this.onHoldCount = this.response.data.onHoldCount;
-//         this.temp = [this.data]; 
-//         cb(this.data);
-//       }
-//       else {
-//         this.toastr.error(this.response.message, 'Message.');
-//       }
-//       // this.spinner.hide();
-//     }, err => {
-//       if (err.status == 400) {
-//         this.toastr.error(err.error.message, 'Message.');;
-//       }
-//       //  this.spinner.hide();
-//     });
-// }
-
-
+      if (this.response.success == true) {
+        this.data = this.response.data.list;
+        this.allCount = this.response.data.allCount;
+        this.openCount = this.response.data.openCount;
+        this.closedCount = this.response.data.closedCount;
+        this.billAwaitedCount = this.response.data.billAwaitedCount;
+        this.billedCount = this.response.data.billedCount;
+        this.receivableCount = this.response.data.receivableCount;
+        this.receivedCount = this.response.data.receivedCount;
+        this.onHoldCount = this.response.data.onHoldCount;
+        this.temp = [this.data]; 
+        cb(this.data);
+      }
+      else {
+        this.toastr.error(this.response.message, 'Message.');
+      }
+      // this.spinner.hide();
+    }, err => {
+      if (err.status == 400) {
+        this.toastr.error(err.error.message, 'Message.');;
+      }
+      //  this.spinner.hide();
+    });
+}
 
 
 
-// deleteContract(obj) {
-//   Swal.fire({
-//     title: GlobalConstants.deleteTitle, //'Are you sure?',
-//     text: GlobalConstants.deleteMessage + ' ' + '"' + obj.autoContractNumber + '"',
-//     icon: 'error',
-//     showCancelButton: true,
-//     confirmButtonColor: '#ed5565',
-//     cancelButtonColor: '#dae0e5',
-//     cancelButtonText: 'No',
-//     confirmButtonText: 'Yes',
-//     reverseButtons: true,
-//     position: 'top',
-//   }).then((result) => {
-//     if (result.isConfirmed) {
 
-//       this.http.delete(`${environment.apiUrl}/api/Contracts/DeleteContract/` + obj.id)
-//         .subscribe(
-//           res => {
-//             this.response = res;
-//             if (this.response.success == true) {
-//               this.toastr.error(this.response.message, 'Message.');
-//               // this.getAllEnquiryItems();
-//               this.fetch((data) => {
-//                 this.rows = data;
-//               });
+
+deleteContract(obj) {
+  Swal.fire({
+    title: GlobalConstants.deleteTitle, //'Are you sure?',
+    text: GlobalConstants.deleteMessage + ' ' + '"' + obj.autoContractNumber + '"',
+    icon: 'error',
+    showCancelButton: true,
+    confirmButtonColor: '#ed5565',
+    cancelButtonColor: '#dae0e5',
+    cancelButtonText: 'No',
+    confirmButtonText: 'Yes',
+    reverseButtons: true,
+    position: 'top',
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+      this.http.delete(`${environment.apiUrl}/api/Contracts/DeleteContract/` + obj.id)
+        .subscribe(
+          res => {
+            this.response = res;
+            if (this.response.success == true) {
+              this.toastr.error(this.response.message, 'Message.');
+              // this.getAllEnquiryItems();
+              this.fetch((data) => {
+                this.rows = data;
+              });
               
 
-//             }
-//             else {
-//               this.toastr.error(this.response.message, 'Message.');
-//             }
+            }
+            else {
+              this.toastr.error(this.response.message, 'Message.');
+            }
 
-//           }, err => {
-//             if (err.status == 400) {
-//               this.toastr.error(this.response.message, 'Message.');
-//             }
-//           });
+          }, err => {
+            if (err.status == 400) {
+              this.toastr.error(this.response.message, 'Message.');
+            }
+          });
 
-//     }
-//   })
+    }
+  })
 
-// }
+}
 
 
 }
