f56524ab476ab53e2ee1a23682950bf70e55d0fd
Samanrafiq <samanrafique668@gmail.com>
2021-06-15 03:33:59 -0700

    changes

--- src/app/Yarn Local/contracts/dispatched-register/dispatched-register.component.ts
+++ src/app/Yarn Local/contracts/dispatched-register/dispatched-register.component.ts
@@ -7,7 +7,8 @@ import { ToastrService } from 'ngx-toastr';
 import { GlobalConstants } from 'src/app/Common/global-constants';
 import { environment } from 'src/environments/environment';
 import Swal from 'sweetalert2';
-import {EditDispatchComponent} from '../dispatched-register/edit-dispatch/edit-dispatch.component'
+// import {EditDispatchComponent} from '../dispatched-register/edit-dispatch/edit-dispatch.component'
+import {DispatchRegisterComponent} from '../yarn-active-contracts/active-contract-details/active-contract-models/dispatch-register/dispatch-register.component'
 @Component({
   selector: 'app-dispatched-register',
   templateUrl: './dispatched-register.component.html',
@@ -19,6 +20,8 @@ export class DispatchedRegisterComponent implements OnInit {
   rows: any = [];
   columns: any = [];
   temp: any[];
+  paymentFilter: any[];
+
   constructor(
     private router: Router,
     private spinner: NgxSpinnerService,
@@ -33,7 +36,15 @@ export class DispatchedRegisterComponent implements OnInit {
       this.rows = data;
     });
   }
-
+  search(event) {
+    const val = event.target.value.toLowerCase();
+    const temp = this.paymentFilter.filter(function (d) {
+      return (d.buyerName.toLowerCase().indexOf(val) !== -1 || 
+       
+      !val);
+    });
+    this.rows = temp;
+  }
   
   allDispatchces(){
     console.log("allDispatchces")
@@ -60,12 +71,15 @@ export class DispatchedRegisterComponent implements OnInit {
 
   fetch(cb) {
     this.http
-      .get(`${environment.apiUrl}/api/YarnContracts/GetAllDispatchRegister`)
+      .get(`${environment.apiUrl}/api/YarnContracts/GetAllDispatchRegisterList`)
       .subscribe(res => {
         this.response = res;
   
         if (this.response.success == true) {
           this.data = this.response.data
+      this.temp = [...this.data]; 
+        
+
           cb(this.data)
         }
         else {
@@ -87,7 +101,7 @@ export class DispatchedRegisterComponent implements OnInit {
   deleteContract(obj) {
     Swal.fire({
       title: GlobalConstants.deleteTitle, //'Are you sure?',
-      text: GlobalConstants.deleteMessage + ' ' + '"' + obj.autoContractNumber + '"',
+      text: GlobalConstants.deleteMessage + ' ' + '"' + obj.contractNo + '"',
       icon: 'error',
       showCancelButton: true,
       confirmButtonColor: '#ed5565',
@@ -129,21 +143,26 @@ export class DispatchedRegisterComponent implements OnInit {
   
   }
    
-  editDispatch(rows) {
-    const modalRef = this.modalService.open(EditDispatchComponent , { centered: true });
-    modalRef.componentInstance.dispatchId = rows.id;
-
+  editDispatch( row ,check) {
+    const modalRef = this.modalService.open(DispatchRegisterComponent, { centered: true });
+    modalRef.componentInstance.statusCheck = check;
+    modalRef.componentInstance.dispatchId = row.dispatchId ;
+    modalRef.componentInstance.contractId = row.contractId ;
+    modalRef.componentInstance.buyerName = row.buyerName ;
+  modalRef.componentInstance.sellerName = row.sellerName ;
+  modalRef.componentInstance.contractNmbr = row.contractNo ;
+  
     modalRef.result.then((data) => {
       // on close
-      // this.fetch((data) => {
-      //   this.rows = data;
-    
-      // });
-      
       if (data == true) {
-     
-
+        this.fetch((data) => {
+          this.temp = [...data]; 
+          this.rows = data;
+        });
       }
+    
+  
+  
     }, (reason) => {
       // on dismiss
     });
