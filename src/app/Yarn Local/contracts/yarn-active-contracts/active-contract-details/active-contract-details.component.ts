6f4fa80f05919577f27fb99ebfbfbca4c1553232
mluqmanbabar124 <68840838+mluqmanbabar124@users.noreply.github.com>
2021-08-09 17:59:44 +0500

    Update active-contract-details.component.ts

--- src/app/Yarn Local/contracts/yarn-active-contracts/active-contract-details/active-contract-details.component.ts
+++ src/app/Yarn Local/contracts/yarn-active-contracts/active-contract-details/active-contract-details.component.ts
@@ -2619,7 +2619,7 @@ getImage(){
                         table:{headerRows: 1 , widths:['20%' , '80%'],
                       body: [
                         [{text: this.contractCommissionData['commissionAmountFabcot'] == 0 ? " " :  'Commission:'  , style:'heading'} ,
-                         {text:   this.contractCommissionData['commissionAmountFabcot'] == 0  ? " " : this.contractCommissionData['fabCotCommisionUOMName'] == null ?   this.contractCostingData['rateCurrencyName']+ " " + this.contractCommissionData['commissionAmountFabcot'] +"  ("+ this.contractCommissionData['fabCotCommision'] +" "+" Seller Side Comm." +" )": this.contractCommissionData['fabCotCommision'] + "/ " + this.contractCommissionData['fabCotCommisionUOMName'] + " " +"From Seller Side" , style:'heading2'}],] }
+                         {text:   this.contractCommissionData['commissionAmountFabcot'] != 0  ? this.contractCommissionData['fabCotCommision'] +"%  ("+" Seller Side Comm." +" )" : ""  , style:'heading2'}],] }
                         
                       },
                       {
