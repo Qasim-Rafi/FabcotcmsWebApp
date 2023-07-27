de018c1c521b34a0dafac59e6140280cc2144945
mluqmanbabar124 <68840838+mluqmanbabar124@users.noreply.github.com>
2023-07-27 13:02:13 +0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-    //apiUrl: 'https://cmsfabcot.net/WebApi'
+    apiUrl: 'https://cmsfabcot.net/WebApi'
 
-    apiUrl: 'https://egreatminds.com/WebApi'
+    //apiUrl: 'https://egreatminds.com/WebApi'
   //apiUrl: 'https://test.egreatminds.com/WebApi'
   //apiUrl: '/FabPublish'
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl:'http://localhost:51381'
-   apiUrl:'http://localhost:51381'
+   //apiUrl:'http://localhost:51381'
+   apiUrl: 'https://cmsfabcot.net/WebApi'
 };
 
 /*
