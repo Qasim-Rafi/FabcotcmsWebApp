ae159ba2f6060362926648274fc3662b2a8a2225
mluqmanbabar124 <68840838+mluqmanbabar124@users.noreply.github.com>
2024-08-12 14:26:22 +0500

    Update active-contract-details.component.ts

--- src/app/Yarn Local/contracts/yarn-active-contracts/active-contract-details/active-contract-details.component.ts
+++ src/app/Yarn Local/contracts/yarn-active-contracts/active-contract-details/active-contract-details.component.ts
@@ -889,7 +889,7 @@ lcForm2( check){
             }   
           } 
 if(this.saleInvoice[0].billInvoiceNumber != 0 ){
-  if(this.loggedInDepartmentName !='Fabric Local'){
+  if(this.loggedInDepartmentName !='Fabric Local' && this.loggedInDepartmentName !='Yarn Import'){
           this.isnotEditAble = true
   }
 }
@@ -1153,7 +1153,7 @@ if(this.saleInvoice[0].billInvoiceNumber != 0 ){
             this.contractData = this.response.data;
             
             if(this.contractData.status == "Closed"){
-              if(this.loggedInDepartmentName !='Fabric Local'){
+              if(this.loggedInDepartmentName !='Fabric Local' && this.loggedInDepartmentName !='Yarn Import'){
               this.isnotEditAble =true
               }
 
